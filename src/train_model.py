38444fe92201f05e35ea7b1e27bc85cbb87ab66a
Doan Si Linh <linhluonleo04@gmail.com>
2025-03-12 19:24:18 +0700

    update src

--- src/train_model.py
+++ src/train_model.py
@@ -17,7 +17,7 @@ os.makedirs("./models", exist_ok=True)
 
 
 class DCGANCallback(keras.callbacks.Callback):
-    def on_epoch_end(self, epoch):
+    def on_epoch_end(self, epoch, logs=None):
         if epoch % 10 == 0:
             self.generator.save(f"./models/generator_{epoch}epoch.keras")
             self.discriminator.save(f"./models/discriminator_{epoch}epoch.keras")
